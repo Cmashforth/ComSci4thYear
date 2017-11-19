71d875585c1e2ddaa910077277ccf0b58410716c
NerdyChris <cmashforth@gmail.com>
2017-11-19 21:19:13 +0000

    MM: Added Images needed for report. Load function added for data, LDA deleted plus test.data code also deleted

--- StatsMultivariateMethodCW/courseworkScript.R
+++ StatsMultivariateMethodCW/courseworkScript.R
@@ -25,73 +25,52 @@ save(train.data,train.label,valid.data,valid.label,test.data,test.label, file = 
 
 ########Data is saved, must be read in from now on##################
 
+load(file = "data.txt")
+
 ######Regression########
 res.lm = lm(train.label~.,data=train.data)
-pred.train = ifelse(res.lm$fitted.values <= 0.5,0,1)
-table(train.label,pred.train)
-correct.class.train = sum(diag(table(train.label,pred.train)))/nrow(train.data)
+pred.class = predict(res.lm, valid.data)
+pred.class = ifelse(pred.class >= 0.5, pred.class <- 1, pred.class <- 0)
+corr.class.rate = sum(valid.label == pred.class)/nrow(valid.data)
 
-res.valid = merge(valid.data,test.data, all = TRUE)
-res.labels = c(valid.label,test.label)
+table(pred.class, valid.label)
 
-n.valid = nrow(res.valid)
-yhat.valid = cbind(rep(1,n.valid),data.matrix(res.valid)) %*% res.lm$coefficients
-yhat.valid = predict(res.lm,res.valid)
-pres.valid = ifelse(yhat.valid<= 0.5,0,1)
-table(res.labels,pres.valid)
-
-correct.class.valid = sum(diag(table(res.labels,pres.valid)))/nrow(res.valid)
 
 ######Knn############
 library(class)
-corr.class.rate = rep(NA,20)
-for(k in 1:20){
+corr.class.rate = rep(NA,50)
+for(k in 1:50){
   pred = knn(train.data,valid.data,train.label,k=k)
   corr.class.rate[k] = sum(pred == valid.label)/nrow(valid.data)
-  
 }
 
-plot(1:20,corr.class.rate, type = "l", xlab = "k")
-max(corr.class.rate)
-best.k = which.max(corr.class.rate)
-pred = knn(train.data,test.data,train.label, k = best.k)
-sum(pred == test.label)/nrow(test.data)
+corr.class.rate
+
+plot(1:50,corr.class.rate, type = "l", xlab = "Choice of K", ylab = "Classification Rate")
+
+pred.best = knn(train.data,valid.data,train.label,k=21)
+table(pred.best, valid.label)
+
+corr.class.rate
 
 #####CVA###############
 library(MASS)
-train.lda = lda(train.data,train.label,prior = c(0.5,0.5))
+train.lda = lda(train.data,train.label,prior = rep(0.5,2))
+train.lda
 plot(train.lda)
 
 valid.lda = predict(train.lda,newdata = valid.data)
 names(valid.lda)
-
+valid.lda
 xtab = table(valid.label,valid.lda$class)
 xtab
 
 1 - sum(diag(xtab))/sum(xtab)
 
-test.lda = predict(train.lda,newdata = test.data)
-xtab = table(test.label,test.lda$class)
-1 - sum(diag(xtab))/sum(xtab)
-
-#######LDA################
-train.lda = lda(train.data,train.label)
-plot(train.lda)
-
-valid.lda = predict(train.lda,valid.data)
-xtab = table(valid.label,valid.lda$class)
-xtab
-
-valid.lda$posterior[valid.label != valid.lda$class,]
-1 - sum(diag(xtab))/sum(xtab)
-
-test.lda = predict(train.lda,test.data)
-xtab = table(test.label,test.lda$class)
-xtab
-1 - sum(diag(xtab))/sum(xtab)
 
 ##########QDA#################
 train.qda = qda(train.data,train.label)
+
 
 valid.qda = predict(train.qda,valid.data)
 
@@ -100,15 +79,9 @@ xtab
 
 1 - sum(diag(xtab))/sum(xtab)
 
-test.qda = predict(train.qda,test.data)
-
-xtab = table(test.label,test.qda$class)
-xtab
-1 - sum(diag(xtab))/sum(xtab)
-
 
 #########Trees#############
-set.seed(126)
+
 library(rpart)
 train.tree = cbind(train.label,train.data)
 valid.tree = cbind(valid.label,valid.data)
@@ -117,9 +90,11 @@ valid.tree = cbind(valid.label,valid.data)
 train.rp = rpart(train.label~., data = train.tree, method = "class")
 
 train.rp
-plot(train.rp)
+plot(train.rp,uniform = T, margin = 0.1)
 text(train.rp)
 plotcp(train.rp)
+printcp(train.rp)
+
 
 valid.rp = predict(train.rp,valid.tree, type = "class")
 xtab = table(valid.label,valid.rp)
