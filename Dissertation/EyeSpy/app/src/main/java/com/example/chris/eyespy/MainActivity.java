9f7d3beaf59df8abee8fd44a11833089f411b105
NerdyChris <cmashforth@gmail.com>
2017-11-22 19:13:04 +0000

    Diss: Started code for uploading photos to data storage

--- Dissertation/EyeSpy/app/src/main/java/com/example/chris/eyespy/MainActivity.java
+++ Dissertation/EyeSpy/app/src/main/java/com/example/chris/eyespy/MainActivity.java
@@ -2,17 +2,27 @@ package com.example.chris.eyespy;
 
 
 import android.graphics.Bitmap;
+import android.icu.text.SimpleDateFormat;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -25,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     private Button logInButton;
     private FirebaseAuth mAuth;
     private Button signOutButton;
+    private String mCurrentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,7 +75,17 @@ public class MainActivity extends AppCompatActivity {
     public void takePicture(View view) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            File photoFile = null;
+            try{
+                photoFile = createImageFile();
+            } catch(IOException ex){
+                Toast.makeText(MainActivity.this, "Failed Image Creation",Toast.LENGTH_SHORT).show();
+            }
+            if(photoFile != null){
+                Uri photoURI = FileProvider.getUriForFile(this,"com.example.android.fileprovider",photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
         }
 
     }
@@ -89,13 +110,16 @@ public class MainActivity extends AppCompatActivity {
         updateUI(null);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            mImageView.setImageBitmap(imageBitmap);
-        }
+
+    private File createImageFile() throws IOException{
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName,".jpg",storageDir);
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
     }
+
+
 
 }
